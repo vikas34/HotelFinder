d01b9b23dee25100edee6659886411d89d8030f1
Vikas <vky2334@gmail.com>
2025-06-25 16:23:41 +0530

    complete Add Room Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,41 @@
-import React from 'react'
-import Navbar from './Components/Navbar.jsx'
-import {useLocation, Routes, Route} from 'react-router-dom'
-import Home from './Pages/Home.jsx'
-import Footer from './Components/Footer.jsx'
-import AllRooms from './Pages/AllRooms.jsx'
-import Roomdetails from './Pages/Roomdetails.jsx'
-import MyBookings from './Pages/MyBookings.jsx'
-import HotelReg from './Components/HotelReg.jsx'
-import LayOut from './Pages/hotelOwner/LayOut.jsx'
-import DashBoard from './Pages/hotelOwner/DashBoard.jsx'
-import AddRoom from './Pages/hotelOwner/AddRoom.jsx'
-import ListRoom from './Pages/hotelOwner/ListRoom.jsx'
-
-
+import React from 'react';
+import Navbar from './Components/Navbar.jsx';
+import { useLocation, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home.jsx';
+import Footer from './Components/Footer.jsx';
+import AllRooms from './Pages/AllRooms.jsx';
+import Roomdetails from './Pages/Roomdetails.jsx';
+import MyBookings from './Pages/MyBookings.jsx';
+import HotelReg from './Components/HotelReg.jsx';
+import LayOut from './Pages/hotelOwner/LayOut.jsx';
+import DashBoard from './Pages/hotelOwner/DashBoard.jsx';
+import AddRoom from './Pages/hotelOwner/AddRoom.jsx';
+import ListRoom from './Pages/hotelOwner/ListRoom.jsx';
 
 const App = () => {
+  const isOwnerPath = useLocation().pathname.includes('owner');
 
-  const isOwnerPath = useLocation().pathname.includes("owner");
   return (
-    <div className=''>
-    {!isOwnerPath && <Navbar/>}
-    {false && <HotelReg/>}
-    <div className='min-h-[70vh] '>
-      <Routes>
-        <Route path='/' element= {<Home/>}/>
-        <Route path='/rooms' element= {<AllRooms/>}/>
-        <Route path='/rooms/:id' element= {<Roomdetails />}/>
-        <Route path='/my-bookings' element= {<MyBookings/>}/>
+    <div>
+      {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms />} />
+          <Route path='/rooms/:id' element={<Roomdetails />} />
+          <Route path='/my-bookings' element={<MyBookings />} />
 
-        <Route path='/owner' element={<LayOut/>}> 
-               <Route index element={<DashBoard/>}/>
-               <Route path='add-room' element={<AddRoom/>}/>
-               <Route path='list-room' element={<ListRoom/>}/>
-
-        </Route>
-      
-
-      </Routes>
+          <Route path='/owner' element={<LayOut />}>
+            <Route index element={<DashBoard />} />
+            <Route path='add-room' element={<AddRoom />} />
+            <Route path='list-room' element={<ListRoom />} />
+          </Route>
+        </Routes>
+      </div>
+      <Footer />
     </div>
- 
-     <Footer/>
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/Pages/hotelOwner/LayOut.jsx
+++ client/src/Pages/hotelOwner/LayOut.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import NavBar from '../../Components/hotelOwner/NavBar'
-import Sidebar from '../../Components/hotelOwner/Sidebar'
-import DashBoard from './DashBoard'
+import React, { useState } from 'react';
+import NavBar from '../../Components/hotelOwner/NavBar';
+import Sidebar from '../../Components/hotelOwner/Sidebar';
+
+import { Outlet } from 'react-router-dom';
 
 const LayOut = () => {
+
+
   return (
-    <div className='flex flex-col h-screen'> 
+    <div className='flex flex-col h-screen'>
       <NavBar/>
       <div className='flex h-full'>
         <Sidebar/>
-        <div className='flex-1 p-4 pt-10 md:px-10 h-full text-orange-400 '>
-            <DashBoard/>
+        <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
+          <Outlet/>
 
         </div>
-      </div>
-    </div>
-  )
-}
 
-export default LayOut
+      </div>
+
+    </div>
+  );
+};
+
+export default LayOut;
