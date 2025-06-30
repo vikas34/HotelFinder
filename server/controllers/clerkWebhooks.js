26bdc78078df86feefc914c5dd74e098bfeaa88e
Vikas <vky2334@gmail.com>
2025-06-30 18:57:12 +0530

    Connect DataBase &  Setup Clerk authentication in Backend

---
+++ server/controllers/clerkWebhooks.js
@@ -0,0 +1,57 @@
+import { json } from "express";
+import User from "../models/user.js";
+import { Webhook } from "svix";
+
+const clerkWebhooks = async (req, res) => {
+  try {
+    //Create a svix instance with clerk webhook secret
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    //Getting Headers
+    const headers = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    };
+
+    //Verifying Header
+
+    await whook.verify(json.stringify(req.body), headers);
+
+    //Getting Data from Request Body
+
+    const { data, type } = req.body;
+
+    const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address,
+      username: data.first_name + "" + data.last_name,
+      image: data.image_url,
+    };
+
+    //Switch cases for different Events
+    switch (type) {
+      case "user.created": {
+        await User.create(userData);
+        break;
+      }
+
+      case "user.updated": {
+        await User.findByIdAndUpdate(data.id, userData);
+        break;
+      }
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        break;
+      }
+      default:
+        break;
+    }
+    res.json({ success: true, message: "webhook Received" });
+  } catch (err) {
+    console.log(err.message);
+    res.json({ success: false, message: err.message })
+  }
+};
+
+export default clerkWebhooks;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
-import express from "express"
-import "dotenv/config"
-import cors from "cors"
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
+connectDB();
 
-const app = express()
-app.use(cors()) //Enable cross origin Resourse Sharing
+const app = express();
+app.use(cors()); //Enable cross origin Resourse Sharing
 
-app.get("/", (req,res)=> res.send("Api is Working"))
+//MiddleWare
+app.use(express.json());
+app.use(clerkMiddleware());
 
-const PORT = process.env.PORT || 8000
+//api to listen to clerk Webhooks
 
-app.listen(PORT, ()=>{
-    console.log(`server running on PORT No ${PORT}`)
-})
+app.use("/api/clerk", clerkWebhooks)
+
+app.get("/", (req, res) => res.send("Api is Working fine"));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`server running on PORT No ${PORT}`);
+});
